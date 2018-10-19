a771037d3307e79dfa2fe9aedc4821921ad5e1d0
eapple <softwrengr@gmail.com>
2018-10-19 11:02:47 +0500

    started

---
+++ app/src/main/java/techease/com/gethelp/MainActivity.java
@@ -0,0 +1,13 @@
+package techease.com.gethelp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
